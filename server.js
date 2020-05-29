fe63d69bd338ab3e05265815b18cf381229c9e9c
Pierre Godichal <godichal.pierre@gmail.com>
2020-05-29 15:45:50 +0200

    ensure server is running fine

--- server.js
+++ server.js
@@ -4,8 +4,22 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const dotenv = require('dotenv');
+const path = require('path');
+
 const app = express();
 
+if (process.env.NODE_ENV) {
+    dotenv.config(
+        {
+            path: path.resolve(process.cwd(), '.env') +
+                '.' + process.env.NODE_ENV
+        }
+    );
+}
+else {
+    dotenv.config();
+}
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -13,15 +27,23 @@ app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/index.html");
 });
 
 app.get('/*', (req, res) => {
-	res.sendFile(__dirname + '/' + req.params[0]);
+    res.sendFile(__dirname + '/' + req.params[0]);
 });
 
+
+const port = process.env.PORT ||
+    process.env.SERVER_PORT ||
+    8443;
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(port, () => {
+    console.log("Your app is listening on port " + listener.address().port);
 });
+
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
