f7be6be57b848ec40b934c20a4b6f765a644e6c3
Pierre Godichal <godichal.pierre@gmail.com>
2020-05-31 00:40:03 +0200

    use random colors in URL

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 "use strict";
+function pickTextColorBasedOnBgColorAdvanced(bgColor, lightColor, darkColor) {
+    var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    var uicolors = [r / 255, g / 255, b / 255];
+    var c = uicolors.map((col) => {
+        if (col <= 0.03928) {
+            return col / 12.92;
+        }
+        return Math.pow((col + 0.055) / 1.055, 2.4);
+    });
+    var L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+    return (L > 0.179) ? darkColor : lightColor;
+}
 
 function dice_initialize(container) {
     $t.remove($t.id('loading_text'));
@@ -16,12 +31,12 @@ function dice_initialize(container) {
 
     function on_set_change(ev) { set.style.width = set.value.length + 3 + 'ex'; }
     $t.bind(set, 'keyup', on_set_change);
-    $t.bind(set, 'mousedown', function(ev) { ev.stopPropagation(); });
-    $t.bind(set, 'mouseup', function(ev) { ev.stopPropagation(); });
-    $t.bind(set, 'focus', function(ev) { $t.set(container, { class: '' }); });
-    $t.bind(set, 'blur', function(ev) { $t.set(container, { class: 'noselect' }); });
+    $t.bind(set, 'mousedown', function (ev) { ev.stopPropagation(); });
+    $t.bind(set, 'mouseup', function (ev) { ev.stopPropagation(); });
+    $t.bind(set, 'focus', function (ev) { $t.set(container, { class: '' }); });
+    $t.bind(set, 'blur', function (ev) { $t.set(container, { class: 'noselect' }); });
 
-    $t.bind($t.id('clear'), ['mouseup', 'touchend'], function(ev) {
+    $t.bind($t.id('clear'), ['mouseup', 'touchend'], function (ev) {
         ev.stopPropagation();
         set.value = '0';
         on_set_change();
@@ -34,25 +49,34 @@ function dice_initialize(container) {
         info_div.style.display = 'none';
         $t.id('control_panel').style.display = 'none';
     }
-    if (params.shadows == 0) {
+    if (params.shadows === 0) {
         $t.dice.use_shadows = false;
     }
-    if (params.color == 'white') {
+    if (params.color === 'white') {
         $t.dice.dice_color = '#808080';
         $t.dice.label_color = '#202020';
     }
-    else if (params.color == 'red') {
+    else if (params.color === 'red') {
         $t.dice.dice_color = '#703030';
     }
-    else if (params.color ){
-        $t.dice.dice_color = '#'+params.color;
+    else if (params.color === 'random') {
+        let colors = [
+            '#703030', '#9e7e46', '#34755e', '#3b6b82', '#47367a', '#622e6e', '#3d3d3d',
+            '#db8a8a', '#cca664', '#70d4b0', '#76b0cc', '#9198e3', '#c282d1', '#c2c2c2'];
+        let rnd = Math.round(Math.random() * (colors.length - 1));
+        $t.dice.dice_color = colors[rnd] || '#3d3d3d';
+        $t.dice.label_color = pickTextColorBasedOnBgColorAdvanced($t.dice.dice_color, '#a0a0a0', '#303030');
+
     }
-      console.log(params);
+    else if (params.color) {
+        $t.dice.dice_color = '#' + params.color;
+    }
+    console.log(params);
 
     var box = new $t.dice.dice_box(canvas, { w: 500, h: 300 });
     box.animate_selector = false;
 
-    $t.bind(window, 'resize', function() {
+    $t.bind(window, 'resize', function () {
         canvas.style.width = window.innerWidth - 1 + 'px';
         canvas.style.height = window.innerHeight - 1 + 'px';
         box.reinit(canvas, { w: 500, h: 300 });
@@ -84,8 +108,8 @@ function dice_initialize(container) {
             if (notation.constant > 0) res += ' +' + notation.constant;
             else res += ' -' + Math.abs(notation.constant);
         }
-        if (result.length > 1) res += ' = ' + 
-                (result.reduce(function(s, a) { return s + a; }) + notation.constant);
+        if (result.length > 1) res += ' = ' +
+            (result.reduce(function (s, a) { return s + a; }) + notation.constant);
         label.innerHTML = res;
         info_div.style.display = 'inline-block';
     }
@@ -93,15 +117,15 @@ function dice_initialize(container) {
     box.bind_mouse(container, notation_getter, before_roll, after_roll);
     box.bind_throw($t.id('throw'), notation_getter, before_roll, after_roll);
 
-    $t.bind(container, ['mouseup', 'touchend'], function(ev) {
+    $t.bind(container, ['mouseup', 'touchend'], function (ev) {
         ev.stopPropagation();
-        if (selector_div.style.display == 'none') {
+        if (selector_div.style.display === 'none') {
             if (!box.rolling) show_selector();
             box.rolling = false;
             return;
         }
         var name = box.search_dice_by_mouse(ev);
-        if (name != undefined) {
+        if (name !== undefined) {
             var notation = $t.dice.parse_notation(set.value);
             notation.set.push(name);
             set.value = $t.dice.stringify_notation(notation);
